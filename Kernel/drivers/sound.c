982df553b587f16f03d49448f53907baa5a213ce
Dalila Orbaj <80479835+dalilaorbaj@users.noreply.github.com>
2025-05-29 19:39:21 -0300

    Update sound.c

---
+++ Kernel/drivers/sound.c
@@ -0,0 +1,35 @@
+//Fuente: https://wiki.osdev.org/PC_Speaker
+#include <stdint.h>
+#include <sound.h>
+
+//Play sound using built-in speaker
+ static void play_sound(uint32_t nFrequence) {
+ 	uint32_t Div;
+ 	uint8_t tmp;
+ 
+        //Set the PIT to the desired frequency
+ 	Div = 1193180 / nFrequence;
+ 	out(0x43, 0xb6);
+ 	out(0x42, (uint8_t) (Div) );
+ 	out(0x42, (uint8_t) (Div >> 8));
+ 
+        //And play the sound using the PC speaker
+ 	tmp = in(0x61);
+  	if (tmp != (tmp | 3)) {
+ 		out(0x61, tmp | 3);
+ 	}
+ }
+ 
+ //make it shut up
+ static void nosound() {
+ 	uint8_t tmp = in(0x61) & 0xFC;
+     
+ 	out(0x61, tmp);
+ }
+ 
+ //Make a beep
+ void beep(uint32_t freq, int time) {
+ 	 play_sound(freq);
+ 	 sleepTicks(time);
+ 	 nosound();
+}
